aa5e137cf93caca634e8380fbe9df355c405951b
lucyoz <lucyoz.kwon@gmail.com>
2022-12-19 11:23:42 +0900

    feat: 특정 fromId 이후의 채팅메시지 조회 GET /chat/messages?fromId=2 는 3번부터 조회 GET /chat/messages 는 전체조회

--- src/main/java/com/likelion/app_chat/ChatController.java
+++ src/main/java/com/likelion/app_chat/ChatController.java
@@ -2,14 +2,18 @@ package com.likelion.app_chat;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
@@ -36,10 +40,41 @@ public class ChatController {
         return new RsData("S-1","메세지가 작성되었습니다.", new WriteMessageResponse(message.getId()));
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class MessagesRequest{
+        //@RequestParam(defaultValue = "0")
+        private final Long fromId;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MessagesResponse{
+        private final List<ChatMessage> messages;
+        private long count;
+    }
+
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<List<ChatMessage>> messages(){
+    public RsData<MessagesResponse> messages(MessagesRequest req){
+        List<ChatMessage> messages = chatMessages;
 
-        return new RsData("S-1","성공",chatMessages );
+        //번호가 입력되었다면
+        if(req.fromId!=null){
+            //해당 번호의 채팅메세지가 전체 리스트에서의 배열 인덱스 번호를 구한다.
+            //없다면 -1
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+            if (index!=-1){
+                //만약에 index가 있다면, 0번 부터 index번까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
+        return new RsData("S-1","성공",new MessagesResponse(messages, messages.size()) );
     }
+
+
 }
