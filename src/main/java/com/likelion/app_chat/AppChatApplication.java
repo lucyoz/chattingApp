63268bc6a107ae27ee60248acae1a9d180b405b6
lucyoz <lucyoz.kwon@gmail.com>
2022-12-19 10:14:20 +0900

    first commit

---
+++ src/main/java/com/likelion/app_chat/AppChatApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.app_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppChatApplication.class, args);
+	}
+
+}
